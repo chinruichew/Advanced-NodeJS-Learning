c5215273d0c7573f1dc7ecc62e7be1dd5b0a459b
Chin Rui <chewchinrui@gmail.com>
2018-04-05 23:21:47 +0800

    Added cluster implementation

--- Enhancement/index.js
+++ Enhancement/index.js
@@ -1,8 +1,30 @@
+process.env.UV_THREADPOOL_SIZE = 1;
+
 const express = require('express');
-const app = express();
+const cluster = require('cluster');
+const crypto = require('crypto');
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
+// Check if file is in master mode
+if(cluster.isMaster) {
+    // cause index.js to be executed again but in child mode
+    cluster.fork();
+    cluster.fork();
+    cluster.fork();
+    cluster.fork();
+} else {
+    // Child which will act like a server
+    const app = express();
 
-app.listen(3000);
+    app.get('/', (req, res) => {
+        // Time Delay
+        crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+            res.send('Hello World!');
+        });
+    });
+
+    app.get('/fast', (req, res) => {
+        res.send('That was really fast!');
+    });
+
+    app.listen(3000);
+}
