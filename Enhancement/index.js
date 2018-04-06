b944441ab46607aac9fe566a853cd81e4a3f5c9b
Chin Rui <chewchinrui@gmail.com>
2018-04-06 18:07:36 +0800

    Add Webworker Threads implementation

--- Enhancement/index.js
+++ Enhancement/index.js
@@ -1,18 +1,32 @@
 const express = require('express');
 const crypto = require('crypto');
+const Worker = require('webworker-threads').Worker;
 
 // Child which will act like a server
 const app = express();
 
 app.get('/', (req, res) => {
-    // Time Delay
-    crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
-        res.send('Hello World!');
+    const worker = new Worker(function() {
+        this.onmessage = function() {
+            let counter = 0;
+            while(counter < 1e9) {
+                counter++;
+            }
+
+            postMessage(counter);
+        };
     });
+
+    worker.onmessage = function(message) {
+        console.log(message.data);
+        res.send(String(message.data));
+    };
+
+    worker.postMessage();
 });
 
 app.get('/fast', (req, res) => {
     res.send('That was really fast!');
 });
 
-app.listen(3000);
+app.listen(3001);
