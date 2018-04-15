cc9dcd769aab5e997c6f245287cded9afee92cf8
Chin Rui <chewchinrui@gmail.com>
2018-04-15 23:14:12 +0800

    Finished browser script flow

--- Advanced/tests/header.test.js
+++ Advanced/tests/header.test.js
@@ -1,17 +1,22 @@
 const puppeteer = require('puppeteer');
 
-test('Add two numbers', () => {
-    const sum = 1 + 2;
-    expect(sum).toEqual(3);
+let browser, page;
+
+// Executed automatically before page runs
+beforeEach(async() => {
+    browser = await puppeteer.launch({
+        headless: false
+    });
+    page = await browser.newPage();
+    await page.goto('http://localhost:3000');
+});
+
+// Executed automatically after page runs
+afterEach(async() => {
+    await browser.close();
 });
 
 test('We can launch a browser', async() => {
-    const browser = await puppeteer.launch({
-        headless: false
-    });
-    const page = await browser.newPage();
-    await page.goto('http://localhost:3000');
-
     const text = await page.$eval('a.brand-logo', el => el.innerHTML);
     expect(text).toEqual('Blogster');
 });
